c462b2e4e3df4060e0c60a1de9a39c4c188c9f77
Hyunsoo Kang <hskang9308@gmail.com>
2021-09-08 15:15:28 +0900

    2021-09-08 : refactoring

--- Algorithm/Programmers/lv1/제일작은수제거하기/solution.js
+++ Algorithm/Programmers/lv1/제일작은수제거하기/solution.js
@@ -1,12 +1,11 @@
 function solution(arr) {
-  const min = Math.min(...arr)
-
-  const find = arr.findIndex(num => num === min)
-
-  arr.splice(find, 1)
-
-  if (arr.length > 0) {
-    return arr
-  }
-  return [-1]
+  // Math.min을 사용하여 가장 작은 수를 뽑는다.
+  let minNum = Math.min(...arr);
+  console.log(minNum);
+  // minNum의 인덱스를 뽑아낸다.
+  let minIndex = arr.findIndex(min => min === minNum);
+  // minIndex 를 제거한다.
+  let result = arr.splice(minIndex, 1);
+  if (arr.length > 0) return arr;
+  else return [-1];
 }
