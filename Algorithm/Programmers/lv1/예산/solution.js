cba223016b00a5c61e3d72bd2d16656d29c7bc0a
Hyunsoo Kang <hskang9308@gmail.com>
2021-09-08 17:48:01 +0900

    2021-09-08 : refactoring with for loop

--- Algorithm/Programmers/lv1/예산/solution.js
+++ Algorithm/Programmers/lv1/예산/solution.js
@@ -1,23 +1,25 @@
+// < method 2 : 반복문 풀이 >
+// d 배열 요소의 총합이 budget과 같거나 작으면 그대로 d.length를 리턴함
+// 그렇지 않다면,
+// d를 내림차순으로 정렬하고
+// 요소를 차례대로 result 배열에 넣음
+// result 배열의 요소들을 더하는 sum 변수를 만들고,
+// 요소의 합이 budget과 같아지면 while문 탈출
+// result배열의 길이를 리턴
 function solution(d, budget) {
-  // d 배열의 합이 budget과 작거나 같으면 그대로  d.length 리턴
-  // d 배열의 합이 budget보다 크면 ...d.sort() 오름차순으로 정렬하고
-  let result = [];
-  // result에 push하고 result의 총합이 budget을 초과하는 경우 result.length 를 리턴한다.
-  let sum = result.reduce((a, b) => a + b, 0)  // sum = 6
-  if (sum > budget) {
-    let sum = result.reduce((a, b) => a + b, 0)  // sum = 6
-
-    return result.length
-  }
+  let dSum = d.reduce((a, b) => a + b, 0);
+  if (dSum <= budget) return d.length;
   else {
-    for (let i = 0; i < d.length; i++) {
-      let sorted = d.sort(); // [1, 2, 3, 4, 5];
-      result.push(sorted[i]); // result = [1, 2, 3]
+    let sorted = d.sort((a, b) => a - b); // [5, 4, 3, 2, 1]
+    let result = [];
+    let sum = 0;
+    let i = 0;
+    for (let i = 0; i < sorted.length; i++) {
+      sum += sorted[i];
+      result.push(sorted[i]);
+      if (sum > budget) break;
+
     }
-    return result.length;
+    return result.length - 1;
   }
-
 }
-
-let output = solution([1, 3, 2, 5, 4], 9);
-console.log(output);
